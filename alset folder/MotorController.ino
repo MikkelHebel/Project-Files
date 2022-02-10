de6c7869e851551b6fdb036b2b4cf5ea03ee8195
HundenDK <43777517+HundenDK@users.noreply.github.com>
2022-02-10 10:16:51 +0100

    Add files via upload

--- alset folder/MotorController.ino
+++ alset folder/MotorController.ino
@@ -1,25 +1,24 @@
 // MotorController
-// Version: 0.2
-
+// Version: 0.3
 #include "Arduino.h"
-char debug = false;
+char debug = true;
 
 // Motor A
-int ENA = 25;
-int IN1 = 13;
-int IN2 = 12;
+int ENA = 36;
+int IN1 = 15;
+int IN2 = 2;
 
 // Motor B
-int ENB = 26;
-int IN3 = 14;
-int IN4 = 27;
+int ENB = 39;
+int IN3 = 0;
+int IN4 = 4;
 
 void setup () {
   Serial.begin(115200);
   Serial.println("[SETUP] Beginning setup!");
+  Serial.print("[DEBUG] debug mode: ");
+  Serial.println(debug);
   // Alle ben er udgange
-  pinMode (ENA, OUTPUT);
-  pinMode (ENB, OUTPUT);
   pinMode (IN1, OUTPUT);
   pinMode (IN2, OUTPUT);
   pinMode (IN3, OUTPUT);
@@ -28,21 +27,31 @@ void setup () {
 }
 
 void loop () {
-  while (debug == true) {
+  if (debug == true) {
     Serial.println("[DEBUG] Running loop!");
   }
 
-  //Retning motor A
-  digitalWrite (IN1, HIGH);
-  digitalWrite (IN2, LOW);
+  // Move the DC motor forward at maximum speed
+  Serial.println("[INFO] Moving Forward");
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  delay(5000);
 
-  //Hastighed motor A
-  digitalWrite(ENA, 0);
+  // Move DC motor backwards at maximum speed
+  Serial.println("[INFO] Moving Backwards");
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  delay(5000);
 
-  //Retning motor B
-  digitalWrite (IN3, HIGH);
-  digitalWrite (IN4, LOW);
-
-  //Hastighed motor B
-  //ledcWrite(ENB, 75);
+  // Stop the DC motor
+  Serial.println("[INFO] Motor stopped");
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  delay(5000);
 }
